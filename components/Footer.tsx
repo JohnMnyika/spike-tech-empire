dfb5386843e991c9c9409a9e6ef0a9e0c8ec4a55
Spike <spike@localhost.localdomain>
2024-10-23 01:44:57 +0300

    fixed the footer of the page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,11 @@
 // components/Footer.tsx
 export default function Footer() {
     return (
-      <footer className="bg-gray-800 text-white py-4">
-        <div className="container mx-auto text-center">
-          <p>&copy; {new Date().getFullYear()} Spike Tech Empire. All Rights Reserved.</p>
-        </div>
-      </footer>
+      <footer className="bg-gray-800 text-white py-4 mt-auto">
+      <div className="container mx-auto text-center">
+        <p>&copy; {new Date().getFullYear()} Spike Tech Empire. All Rights Reserved.</p>
+      </div>
+    </footer>
     );
   }
   
